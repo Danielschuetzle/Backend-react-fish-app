0737b5b87c48e389e9f716e7ebb79d258877f4b9
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-07 16:41:00 +0200

    post logic added

--- components/ProductForm/index.js
+++ components/ProductForm/index.js
@@ -1,14 +1,52 @@
+import { useState } from 'react';
+import { mutate } from 'swr';
 import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
 import { StyledButton } from "../Button/Button.styled";
 
 export default function ProductForm() {
+  // Using React's useState hook to manage the loading state
+  const [loading, setLoading] = useState(false);
+
+  // This async function handles the form submission event
   async function handleSubmit(event) {
+    // Prevents the page from reloading upon form submission
     event.preventDefault();
 
+    // Retrieves form data and converts it to a key-value object
     const formData = new FormData(event.target);
     const productData = Object.fromEntries(formData);
+
+    // Indicate that loading has started
+    setLoading(true);
+
+    try {
+      // Makes a POST request to add a new product
+      const response = await fetch('/api/products', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(productData), // Sends product data as JSON
+      });
+
+      // If response is not ok (status not 2xx), throw an error
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      // Once new data is added, call mutate to revalidate the data from the SWR cache
+      mutate('/api/products');
+      
+    } catch (error) {
+      // If there's an error, log it to the console
+      console.error('Failed to add product:', error);
+    } finally {
+      // Whether the request succeeded or failed, indicate that loading has finished
+      setLoading(false);
+    }
   }
 
+  // The return statement defines the component's UI
   return (
     <StyledForm onSubmit={handleSubmit}>
       <StyledHeading>Add a new Fish</StyledHeading>
@@ -32,7 +70,9 @@ export default function ProductForm() {
           <option value="GBP">GBP</option>
         </select>
       </StyledLabel>
-      <StyledButton type="submit">Submit</StyledButton>
-    </StyledForm>
+      <StyledButton type="submit" disabled={loading}>
+        {loading ? 'Loading...' : 'Submit'} {/* Shows 'Loading...' if loading, else 'Submit' */}
+      </StyledButton>
+      </StyledForm>
   );
 }
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -2,10 +2,38 @@ import dbConnect from "../../../db/connect";
 import Product from "../../../db/models/Product";
 
 export default async function handler(request, response) {
+
+  // Connect to the database
   await dbConnect();
 
+  // Check if the HTTP method is 'GET'
   if (request.method === "GET") {
+    // Find all products in the database
     const products = await Product.find();
+    // Return a response with status 200 and the products
     return response.status(200).json(products);
+  }
+  
+  // Check if the HTTP method is 'POST'
+  else if (request.method === "POST") {
+    // Use a try...catch block to handle potential errors
+    try {
+      // Extract product data from the request body
+      const productData = request.body;
+      // Create a new product in the database with the product data
+      const product = await Product.create(productData);
+      // If successful, return a response with status 201 and a success message
+      return response.status(201).json({ status: "Product created." });
+    } catch (error) {
+      // Log any errors to the console
+      console.error(error);
+      // If an error occurred, return a response with status 400 and the error message
+      return response.status(400).json({ error: error.message });
+    }
+  } 
+  // For any other HTTP method, return a response with status 405 (Method Not Allowed)
+  else {
+    response.setHeader("Allow", ["GET", "POST"]);
+    return response.status(405).json({ error: `Method ${request.method} Not Allowed` });
   }
 }
